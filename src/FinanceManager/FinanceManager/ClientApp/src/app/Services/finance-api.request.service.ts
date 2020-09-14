968f1228fa221debdd9adb9ee0ba23fc7f1881e4
Ben Fletcher <ben.fletcher@iress.com>
2020-09-14 16:16:57 +0100

    Added config.json file

--- src/FinanceManager/FinanceManager/ClientApp/src/app/Services/finance-api.request.service.ts
+++ src/FinanceManager/FinanceManager/ClientApp/src/app/Services/finance-api.request.service.ts
@@ -3,6 +3,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { Params } from '@angular/router';
 import { catchError } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
@@ -22,13 +23,27 @@ export class FinanceApiRequest {
   }
 
   public static LoadBaseUrl(http: HttpClient): () => Promise<void> {
-    return () =>
+    return async () => {
+      const config = await http.get<any>('/assets/config.json').toPromise();
+      if (config && config.FinanceApiUrl && config.FinanceApiUrl !== '') {
+        if (!environment.production) {
+          console.log('Using FinanceApiUrl from config.json');
+        }
+        FinanceApiRequest.BASE_URL = config.FinanceApiUrl;
+        return;
+      }
+
+      if (!environment.production) {
+        console.log('Using FinanceApiUrl from backend call');
+      }
+
       http
         .get<string>('/api/config/FinanceApiUrl')
         .toPromise()
         .then((url) => {
           FinanceApiRequest.BASE_URL = url;
         });
+    };
   }
 
   public get<T>(url: string, queryParams?: Params): Observable<T> {
