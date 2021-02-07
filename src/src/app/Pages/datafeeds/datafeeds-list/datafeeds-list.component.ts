831bb75c1b3fb76491917dc88974a8a7944f428a
benfl3713 <buses3713@gmail.com>
2021-02-07 20:24:40 +0000

    Added reconnect button to datafeeds

--- src/src/app/Pages/datafeeds/datafeeds-list/datafeeds-list.component.ts
+++ src/src/app/Pages/datafeeds/datafeeds-list/datafeeds-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { FinanceApiRequest } from 'src/app/Services/finance-api.request.service';
 import { DatafeedsService } from '../../../Services/datafeeds.service';
 
 @Component({
@@ -7,7 +8,10 @@ import { DatafeedsService } from '../../../Services/datafeeds.service';
   styleUrls: ['./datafeeds-list.component.css'],
 })
 export class DatafeedsListComponent implements OnInit {
-  constructor(private datafeedsService: DatafeedsService) {}
+  constructor(
+    private datafeedsService: DatafeedsService,
+    private financeApi: FinanceApiRequest
+  ) {}
 
   @Input() provider: string;
   datafeeds: any[] = [];
@@ -33,5 +37,46 @@ export class DatafeedsListComponent implements OnInit {
         .deleteDatafeed(datafeed.Provider, datafeed.VendorID)
         .subscribe(() => this.loadDatafeeds());
     }
+  }
+
+  reconnectDatafeed(datafeed) {
+    switch (datafeed.Provider) {
+      case 'TRUELAYER':
+        this.reconnectTruelayer(datafeed);
+        break;
+      default:
+        break;
+    }
+  }
+
+  reconnectTruelayer(datafeed) {
+    let baseUrl: string;
+    let clientId = this.financeApi.get<string>(
+      'DatafeedAuth/GetTrueLayerClientId'
+    );
+    let redirectUrl: string = `${FinanceApiRequest.BASE_URL}DatafeedAuth/TrueLayerAuthentication`;
+
+    clientId.subscribe({
+      next: (id) => {
+        baseUrl = id.startsWith('sandbox-')
+          ? 'https://auth.truelayer-sandbox.com/'
+          : 'https://auth.truelayer.com/';
+        console.log(baseUrl);
+        window.open(
+          baseUrl +
+            '?response_type=code&client_id=' +
+            id +
+            '&scope=accounts%20balance%20transactions%20offline_access&redirect_uri=' +
+            redirectUrl +
+            '&response_mode=form_post&providers=uk-ob-all%20uk-oauth-all%20uk-cs-all%20uk-cs-mock&state=' +
+            localStorage.getItem('id_token') +
+            '|' +
+            datafeed._id +
+            `&provider_id=${datafeed.VendorID}`,
+          '_blank'
+        );
+      },
+      error: () => alert('Something went wrong'),
+    });
   }
 }
